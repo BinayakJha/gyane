713fbd2b1093590c38b0fa3546bfb1a56ab21a0c
BinayakJha <jha36binayak@gmail.com>
2022-01-18 06:48:23 +0545

    added mysql db

--- gyane/settings.py
+++ gyane/settings.py
@@ -83,11 +83,14 @@ WSGI_APPLICATION = 'gyane.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': str(os.getenv('DB_NAME')),
+        'USER': str(os.getenv('DB_USER')),
+        'PASSWORD': str(os.getenv('DB_PASSWORD')),
+        'HOST': str(os.getenv('DB_HOST')),
+        'PORT': str(os.getenv('DB_PORT ')),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
